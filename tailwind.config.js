9564bfa976932666898803fdc429d1b5f7f53623
rezamultiaha <takolkaki@gmail.com>
2022-11-23 19:10:01 +0700

    update gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,10 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      }
+        sm: '1rem',
+        lg: '2rem',
+        xl: '3rem',
+      },
     },
     extend: {
       colors: {
